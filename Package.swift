fe3d0f2b353aa74be6eacd9035fed04ca7f9c4e9
runner <runner@Mac-1658247502341.local>
2022-07-19 16:27:37 +0000

    New WishiSDK version 0.1.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let version = "0.1.0"
 let moduleName = "WishiSDK"
-let checksum = "237fee6e10024504fc709c8bba25fdee2f602b929d81249c5a9891b5ae99f5e6"
+let checksum = "a0f0c8c5882a1cae9dd14f9472afb5770c9efbf56dfb6182caf7b864eb495e05"
 
 let package = Package(
     name: moduleName,
