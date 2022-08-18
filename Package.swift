e4fed4df28acebc60a00bded82bf02dac71c8b67
Orenbachar2 <oren@wishi.me>
2022-08-18 16:07:12 +0300

    New package structure

--- Package.swift
+++ Package.swift
@@ -1,28 +1,29 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+let frameworkName = "WishiSDKSPM" //your SPM wrapper, satisfies dependancies
+let binaryName = "WishiSDK"
 import PackageDescription
 
-let version = "0.6.0"
-let moduleName = "WishiSDK"
-let checksum = "0470d4c1a486dbf5d8c3ff880dda481a2c4117aeff8f06eeb3f82e385eeeda43"
-
 let package = Package(
-    name: moduleName,
-    platforms: [
-        .iOS(.v13)
-    ],
+    name: frameworkName,
+    platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: moduleName,
-            targets: [moduleName]
-        )
+            name: frameworkName,
+            targets: [frameworkName]),
+    ],
+    dependencies: [
+      .package(url: "https://github.com/twilio/conversations-ios.git", exact: Version.init(2, 2, 4))
     ],
     targets: [
+      .target(name: frameworkName, dependencies: [
+        .target(name: binaryName),
+        .product(name: "TwilioConversationsClient", package: "conversations-ios")
+      ]),
         .binaryTarget(
-            name: moduleName,
-            url: "https://github.com/wishidev/sdk-ios-spm/releases/download/\(version)/\(moduleName).xcframework.zip",
-            checksum: checksum
+          name: binaryName,
+          path: binaryName + ".xcframework"
         )
     ]
 )
