65df163569c60127d7ab2be7c83dae9ebf14b533
Oren bachar <orenbachar@Orens-MacBook-Pro-2.local>
2022-08-31 13:27:26 +0300

    New version

---
+++ WishiSDK.xcframework/ios-arm64/WishiSDK.framework/Headers/WishiSDK.h
@@ -0,0 +1,16 @@
+//
+//  WishiSDK.h
+//  WishiSDK
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WishiSDK.
+FOUNDATION_EXPORT double WishiSDKVersionNumber;
+
+//! Project version string for WishiSDK.
+FOUNDATION_EXPORT const unsigned char WishiSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WishiSDK/PublicHeader.h>
+
+
